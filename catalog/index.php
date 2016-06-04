f5c91942d93315d823e86cbffcf9ec95414f6934
Mike Petruniak <mike.petruniak@gmail.com>
2016-06-04 09:08:43 -0400

    Removed superfluous content
    Removed the code responsible for displaying the “data is refreshed”
    disclaimer/alert.  This is really only necessary on my personal website.

--- catalog/index.php
+++ catalog/index.php
@@ -1,12 +1,6 @@
 <?php require_once('../common/header.php'); ?>
   <section role="main" id="catalog">
     <div class="container-fluid">
-      <div class="alert alert-info alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-        Data is refreshed every one-hour.  Go ahead and give it a try!
-      </div>
       <div class="row">
         <div class="col-xs-12">
           <div class="box">
